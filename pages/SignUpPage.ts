c687b24849546d60ebe45df1722c2304710f47cb
AZR88 <azrielamaldany@gmail.com>
2026-08-20 16:03:27 +0700

    Add SignUp Page

--- pages/SignUpPage.ts
+++ pages/SignUpPage.ts
@@ -1,22 +1,22 @@
 import { Page, Locator } from '@playwright/test';
 
-export class LoginPage {
+export class SignUpPage {
     private readonly page: Page;
-    private readonly loginNavButton: Locator; 
+    private readonly SignUpNavButton: Locator; 
     private readonly emailInput: Locator;
     private readonly passwordInput: Locator;
     private readonly submitButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.loginNavButton = page.getByRole('link', { name: 'Sign in' });
+        this.SignUpNavButton = page.getByRole('link', { name: 'Sign up' });
         this.emailInput = page.getByPlaceholder('Email');
         this.passwordInput = page.getByPlaceholder('Password');
         this.submitButton = page.getByRole('button', { name: 'Sign in' });
     }
 
     async login(email: string, password: string): Promise<void> {
-        await this.loginNavButton.click();
+        await this.SignUpNavButton.click();
         await this.emailInput.fill(email);
         await this.passwordInput.fill(password);
         await this.submitButton.click();
