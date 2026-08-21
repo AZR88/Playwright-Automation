3c05946c148a6df2ada4582511a16fdfba8b79a5
AZR88 <azrielamaldany@gmail.com>
2026-08-21 23:33:38 +0700

    update

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,5 +1,4 @@
 import { test as setup, expect } from '../fixtures/test-base';
-import { LoginPage } from '../pages/LoginPage';
 
 setup('Autentikasi dan simpan state per browser', async ({ page, loginPage }) => {
     await page.goto('/');
@@ -8,7 +7,7 @@ setup('Autentikasi dan simpan state per browser', async ({ page, loginPage }) =>
     await loginPage.submitLogin();
     await expect(page.getByText('Your Feed')).toBeVisible();
 
-    // Simpan ke 3 file berbeda
+    
     await page.context().storageState({ path: 'playwright/.auth/chromium.json' });
     await page.context().storageState({ path: 'playwright/.auth/firefox.json' });
     await page.context().storageState({ path: 'playwright/.auth/webkit.json' });
